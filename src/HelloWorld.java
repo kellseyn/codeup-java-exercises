4099a5cfbed6459752735e06a4037236117421d8
Kellsey Neagley <kellseyneagley@gmail.com>
2019-11-20 14:04:35 -0600

    completed syntax exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 16;
+        double myString = 3.14159;
+        float myNumber = 3.14f;
+                System.out.println(myNumber);
+
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String class = "Hi";
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//            System.out.println(theNumberThree);
+//            System.out.println(three);
+//
+//        int three = (int) "three";
+//            System.out.println(three);
+
+//        int x = 4;
+//            System.out.println(x + 5);
+
+//        int x = 3;
+//        int y = 4;
+//            System.out.println(y * x);
+
+        int x = 10;
+        int y = 2;
+            System.out.println(x / y);
+            System.out.println(y - x);
+    }
+}
