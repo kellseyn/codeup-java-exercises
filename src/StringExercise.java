e429db26d0f1a4233c1d33b9a3686bb860a2b68c
Kellsey Neagley <kellseyneagley@gmail.com>
2019-11-22 10:13:40 -0600

    completed string exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String education = "We don't need no education";
+        String control = "We don't need no thought control";
+        String look = "Check \"this\" out!, \"s inside of \"s!";
+        String sys = "In windows, the main drive is usually C:\\";
+        String slash = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+
+            System.out.printf("%s%n%s%n%n%s%n%n%s%n%n%s", education, control, sys, look, slash);
+
+
+
+
+
+    }
+}
