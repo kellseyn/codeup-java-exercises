c10ddd83952a02c543ced6a3d3992ea92ffa020d
Kellsey Neagley <kellseyneagley@gmail.com>
2019-12-30 19:34:53 -0600

    completed the 6th day of christmas

--- src/Christmas/Reindeer.java
+++ src/Christmas/Reindeer.java
@@ -1,4 +1,21 @@
 package Christmas;
 
+import java.util.Scanner;
+
 public class Reindeer {
+    protected String name;
+
+
+    public Reindeer(String name) {
+        this.name = name;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Add a reindeer please");
+        String name = sc.nextLine();
+        System.out.printf("You added %s as one of Santa's reindeer!", name);
+    }
 }
--- src/Christmas/Santa.java
+++ src/Christmas/Santa.java
@@ -1,16 +1,54 @@
 package Christmas;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Santa {
+
+    public static Reindeer[] reindeerteam;
+    static {
+        reindeerteam = new Reindeer[9];
+
+        reindeerteam[0] = new Reindeer("Dasher");
+        reindeerteam[1] = new Reindeer("Dancer");
+        reindeerteam[2] = new Reindeer("Prancer");
+        reindeerteam[3] = new Reindeer("Vixen");
+        reindeerteam[4] = new Reindeer("Comet");
+        reindeerteam[5] = new Reindeer("Cupid");
+        reindeerteam[6] = new Reindeer("Donner");
+        reindeerteam[7] = new Reindeer("Blitzen");
+        reindeerteam[8] = new Reindeer("Rudolph");
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("HO HO HO, What would you like for Christmas?");
         String input = sc.nextLine();
         if (input.equalsIgnoreCase("")){
-            System.out.println("It's ok, you can tell me what you want for Christmas");
+            System.out.println("It's ok, you can tell me what you want for Christmas\n");
         } else{
-            System.out.printf("Ahh! so you want a %s for Christmas, I'll put it on the list", input);
+            System.out.printf("Ahh! so you want a %s for Christmas, I'll put it on the list%n", input);
         }
+
+        for (Integer i = 0; i < reindeerteam.length; i++) {
+            System.out.println(reindeerteam[i].name);
+        }
+
+
+
+
+//        System.out.println("The team:\n");
+//        System.out.println(dasher.name);
+//        System.out.println(dancer.name);
+//        System.out.println(prancer.name);
+//        System.out.println(vixen.name);
+//        System.out.println(comet.name);
+//        System.out.println(cupid.name);
+//        System.out.println(donner.name);
+//        System.out.println(blitzen.name);
+//        System.out.println(rudolph.name);
     }
 }
