35ca45bbf618d7de407004be47602f43eec8621f
Kellsey Neagley <kellseyneagley@gmail.com>
2020-01-09 09:25:16 -0600

    completed warmup

---
+++ src/SimpleAdding.java
@@ -0,0 +1,10 @@
+public class SimpleAdding {
+
+    public static int Adding(int num){
+        return (num / 2) * (num + 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Adding(10));
+    }
+}
